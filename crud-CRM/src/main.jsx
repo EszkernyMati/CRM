9a6ceb9e4aaf50190c809052fb05f5bbaea54f0a
EszkernyMati <mateuszpoppe-kaca@elektronikonline.pl>
2026-09-17 21:07:23 +0200

    txt

--- crud-CRM/src/main.jsx
+++ crud-CRM/src/main.jsx
@@ -8,7 +8,7 @@ import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/CRM">
+    <BrowserRouter basename="/CRM/">
       <ToastProvider>
         <AuthProvider>
           <App />
